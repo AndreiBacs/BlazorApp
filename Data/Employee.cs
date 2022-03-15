639d2f22e0b47190d6793da3ad4248f2b74b20e5
Andrei Bacs <andrei.bacs@fortech.ro>
2022-03-15 19:24:16 +0200

    Add project files.

---
+++ Data/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace BlazorApp.Data
+{
+  public class Employee
+  {
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string Department { get; set; }
+
+    [Required]
+    public DateTime EntryDate { get; set; }
+
+    [Required]
+    public List<string> Tasks { get; set; }
+  }
+}
---
+++ Data/EmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using AutoFixture;
+
+namespace BlazorApp.Data
+{
+  public class EmployeeService
+  {
+    public Task<List<Employee>> GetEmployees()
+    {
+      var fixture = new Fixture();
+
+      var employees = fixture.Create<List<Employee>>();
+
+      return Task.FromResult(employees);
+    }
+  }
+}
